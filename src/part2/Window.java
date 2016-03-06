78e82d59b993e6e3dfbd863dcba76de4d9442ab2
jamalz371@gmail.com <Jamal.Ben.Azouze@ulb.ac.be>
2016-03-06 22:03:42 +0100

    Second part

---
+++ src/part2/Window.java
@@ -0,0 +1,13 @@
+package part2;
+
+import javax.swing.JLabel;
+
+public class Window {
+
+	//ImageIcon icon = createImageIcon("images/middle.gif");
+	private JLabel labelIP= new JLabel("IP : ");
+	
+	public Window(){
+		
+	}
+}
