915544ef9a0296d80ac7733878dd076ca0de1a89
jamalz371@gmail.com <Jamal.Ben.Azouze@ulb.ac.be>
2016-04-06 22:33:35 +0200

    KeysTest cleaned

--- src/test/KeysTest.java
+++ src/test/KeysTest.java
@@ -1,13 +1,6 @@
 package test;
 
-import java.awt.RenderingHints.Key;
 import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-
 import part1.AES_128;
 import part1.RSA_2048;
 import part2.SessionKey;
@@ -15,23 +8,21 @@ import part2.SessionKey;
 public class KeysTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SessionKey a = new SessionKey();
-		a.setKeyAES();
-		a.setKeysRSA();
+		SessionKey.setKeyAES();
+		SessionKey.setKeysRSA();
 		System.out.println("Début AES : ");
 		System.out.println("------------");
-		System.out.println("AES key : " + a.getKeyAES());
-		System.out.println("AES key getEncoded : " + a.getKeyAES().getEncoded());
-		System.out.println("AES key getEncoded : " + a.getKeyAES().getEncoded());
-		System.out.println("AES key gethash : " + a.getKeyAES().hashCode());
-		System.out.println("AES key gethash : " + a.getKeyAES().hashCode());
-		System.out.println("AES key : " + a.getKeyAES());
+		System.out.println("AES key : " + SessionKey.getKeyAES());
+		System.out.println("AES key getEncoded : " + SessionKey.getKeyAES().getEncoded());
+		System.out.println("AES key getEncoded : " + SessionKey.getKeyAES().getEncoded());
+		System.out.println("AES key gethash : " + SessionKey.getKeyAES().hashCode());
+		System.out.println("AES key gethash : " + SessionKey.getKeyAES().hashCode());
+		System.out.println("AES key : " + SessionKey.getKeyAES());
 		
 		String message = "Salut tout le monde !";
-		byte[] resAES = AES_128.encrypt(a.getKeyAES().getEncoded(), message.getBytes());
-		byte[] plainAES = AES_128.decrypt(a.getKeyAES().getEncoded(), resAES);
-		java.security.Key[] tmp = a.getKeysRSA();
+		byte[] resAES = AES_128.encrypt(SessionKey.getKeyAES().getEncoded(), message.getBytes());
+		byte[] plainAES = AES_128.decrypt(SessionKey.getKeyAES().getEncoded(), resAES);
+		java.security.Key[] tmp = SessionKey.getKeysRSA();
 		byte[] resRSA = RSA_2048.encrypt(tmp[0].getEncoded(), message.getBytes());
 		byte[] plainRSA = RSA_2048.decrypt(tmp[1].getEncoded(), resRSA);
 		
